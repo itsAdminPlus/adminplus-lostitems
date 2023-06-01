b2572e75fb18cd55af3a2f181e120f049815e89a
OppoNoppo <45967788+OppoNoppo@users.noreply.github.com>
2023-06-01 20:28:45 +0200

    Added menu functions

--- adminplus-lostitems/client/cl_deaditems.lua
+++ adminplus-lostitems/client/cl_deaditems.lua
@@ -7,9 +7,111 @@ Citizen.CreateThread(function()
       if IsEntityDead(PlayerPedId()) then
         TriggerServerEvent('playerDied')
         break
+local function libMenu(data, latest)
+  -- Second Layer
+  local function subMenu(items)
+    local menuItems = {}
+    for _, item in pairs(items) do
+      table.insert(menuItems, {
+        title = item.name,
+        description = item.count
+      })
+    end
+    lib.registerContext({
+      id = 'lostItems-2',
+      title = 'Lost Items',
+      menu = 'lostItems-1',
+      options = {
+        table.unpack(menuItems)
+      }
+    })
+    lib.showContext('lostItems-2')
+  end
+
+  -- First Layer
+  local menuItems = {}
+  -- Set latest first
+  table.insert(menuItems, {
+    title = (GetGameTimer() - latest.time) / 1000..' Seconds Ago',
+    description = 'Latest',
+    onSelect = function() pcall(subMenu, latest.items) end
+  })
+
+  -- Now do Data
+  for i=1, #data do
+    table.insert(menuItems, {
+      title = (GetGameTimer() - data[i].time) / 1000..' Seconds Ago',
+      onSelect = function() pcall(subMenu, data[i].items) end
+    })
+  end
+  lib.registerContext({
+    id = 'lostItems-1',
+    title = 'Lost Items',
+    options = {
+      {
+        title = 'When it gets saved?',
+        description = 'Whenever you die in the period of the serverinstance being open. All items when dying gets saved. Even after when you disconnect yourself! :)',
+        disabled = true
+      },
+      table.unpack(menuItems)
+    }
+  })
+  lib.showContext('lostItems-1')
+end
+
+local function esxMenu(data, latest)
+  local menuItems = {}
+  -- Explain segment
+  table.insert(menuItems, {
+    title = 'When it gets saved?',
+    description = 'Whenever you die in the period of the serverinstance being open. All items when dying gets saved. Even after when you disconnect yourself! :)',
+    unselectable = true,
+  })
+
+  -- Set latest first
+  table.insert(menuItems, {
+    title = (GetGameTimer() - latest.time) / 1000..' Seconds Ago',
+    description = 'Latest',
+    items = latest.items
+  })
+
+  -- Now do Data
+  for i=1, #data do
+    table.insert(menuItems, {
+      title = (GetGameTimer() - data[i].time) / 1000..' Seconds Ago',
+      items = data[i].items
+    })
+  end
+
+  ESX.OpenContext("right" , menuItems,
+    function(menu,element)
+      ESX.CloseContext()
+
+      local menuItems_Sub = {}
+      for _, item in pairs(element.items) do
+        table.insert(menuItems_Sub, {
+          title = item.name,
+          description = item.count,
+          unselectable = true
+        })
       end
+
+      ESX.OpenContext("right" , element.,
+        function(menu,element)
+          -- onSelect function
+        end,
+        function()
+          -- onClose function
+        end
+      end)
+
+
+    end,
+    function()
+      -- onClose function
     end
   end)
+end
 
   RegisterNetEvent('inventoryDataSaved')
 AddEventHandler('inventoryDataSaved', function(characterLicense)
