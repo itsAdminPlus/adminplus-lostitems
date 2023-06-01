cd1c1d31bf79dc454f3d34010b17f6d09dcbeef3
OppoNoppo <45967788+OppoNoppo@users.noreply.github.com>
2023-06-01 20:29:08 +0200

    Cleanup

--- adminplus-lostitems/client/cl_deaditems.lua
+++ adminplus-lostitems/client/cl_deaditems.lua
@@ -1,12 +1,5 @@
 ESX = exports["es_extended"]:getSharedObject()
 
--- Triggered when the player dies
-Citizen.CreateThread(function()
-    while true do
-      Citizen.Wait(0)
-      if IsEntityDead(PlayerPedId()) then
-        TriggerServerEvent('playerDied')
-        break
 local function libMenu(data, latest)
   -- Second Layer
   local function subMenu(items)
@@ -113,19 +106,6 @@ local function esxMenu(data, latest)
   end)
 end
 
-  RegisterNetEvent('inventoryDataSaved')
-AddEventHandler('inventoryDataSaved', function(characterLicense)
-  print('Inventory data saved for character: ' .. characterLicense)
-end)
-
-RegisterNetEvent('showLostItems')
-AddEventHandler('showLostItems', function(message)
-  -- Display chat message with lost items
-  TriggerEvent('chat:addMessage', {
-    color = { 255, 255, 255 },
-    multiline = true,
-    args = { message }
-  })
 RegisterNetEvent('lostItems:goToClient', function(data, latest)
   if not lib then
     esxMenu(data, latest)
